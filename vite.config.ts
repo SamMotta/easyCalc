9bb55c1383a67d9561e262645c46c2d4eb994a7a
SamMotta <samuelsamuca953@gmail.com>
2022-10-23 23:28:05 -0300

    WebManifest created by vite-plugin-pwa SSE: robot.txt added

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,38 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      base: "/",
       registerType: "prompt",
       injectRegister: "auto",
       workbox: {
         clientsClaim: true,
         skipWaiting: true,
-        globPatterns: ["**\/*.{js,css,html,svg}"]
+        globPatterns: ["**\/*.{js,css,html,svg,png,jpeg}"]
       },
 
       devOptions: {
         // enabled: true,
       },
+
+      manifest: {
+        name: "Easy Calculator",
+        short_name: "EasyCalc",
+        theme_color: "#3b82f6",
+        background_color: "#faf7f5",
+        display: "standalone",
+        orientation: "portrait",
+        lang: "pt-br",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "/images/icons/sample-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          }
+        ],
+      }
     })
   ]
 })
